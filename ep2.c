5e9ea5dfbae0c3949a7c9bdae1227d1eefc4b81e
Gustavo Rodrigues Cayres Silva <gustavoc@python.linux.ime.usp.br>
2015-10-27 16:07:33 -0200

    warnings removidos, matrix generator quase pronta

--- ep2.c
+++ ep2.c
@@ -1,8 +1,10 @@
+#include <stdlib.h>
 #include <math.h>
 #include <stdio.h>
 #include <time.h>
 #define nmax 700
-#define E 0.0001 /*erro*/
+#define E 1e-10 /* Acceptable error */
+/* Compilation command: gcc -Wall -Wextra -ansi -pedantic -o ep2 ep2.c -lm */
 
 void swap(double* a, double* b) {
 	double temp;
@@ -12,37 +14,29 @@ void swap(double* a, double* b) {
 }
 
 /* calculates the inner product between vectors v1 and v2 */
-double inner_product(double v1[], double v2[], int size_v) {
+double inner_product(double v1[], double v2[], int size) {
 	int i;
 	double inner_product = 0;
 
-	for (i = 0; i < size_v; i ++)
+	for (i = 0; i < size; i ++)
 		inner_product += v1[i]*v2[i];
 	
 	return inner_product;
 }
 
 /* calculates the multiplication between a matrix and a vector */
-void matrix_vector_product(double A[][nmax], double v[], int size, double aux[]) {
+void matrix_vector_product(double A[][nmax], double v[], int size, double result[]) {
 	int i, j;
 
 	for (i = 0; i < size; i ++) {
-		aux[i] = 0;
+		result[i] = 0;
 		for (j = 0; j < size; j ++)
-			aux[i] += A[i][j]*v[j];
+			result[i] += A[i][j]*v[j];
 	}
 }
 
 /* multiplies a vector by a scalar */
-void vector_scalar_product(double v[], double alfa, int size) {
-	int i;
-
-	for (i = 0; i < size; i ++)
-		v[i] = v[i]*alfa;
-}
-
-/* multiplies a vector by a scalar and puts the result on the 4th argument*/
-void vector_scalar_product_new_vector(double v[], double alfa, int size, double result[]) {
+void vector_scalar_product(double v[], double alfa, int size, double result[]) {
 	int i;
 
 	for (i = 0; i < size; i ++)
@@ -50,24 +44,30 @@ void vector_scalar_product_new_vector(double v[], double alfa, int size, double 
 }
 
 /* adds vectors v1 and v2 */
-void vector_sum(double v1[], double v2[], int size) {
+void vector_sum(double v1[], double v2[], int size, double result[]) {
 	int i;
 
 	for (i = 0; i < size; i ++)
-		v1[i] += v2[i];
+		result[i] = v1[i] + v2[i];
 }
 
 /* subtracts vector v2 from v1 */
-void vector_subtraction(double v1[], double v2[], int size) {
+void vector_subtraction(double v1[], double v2[], int size, double result[]) {
 	int i;
 
 	for (i = 0; i < size; i ++)
-		v1[i] -= v2[i];
+		result[i] = v1[i] - v2[i];
 }
 
-void conjugate_gradient(double A[][nmax], double b[], int size, int n_steps) {
-	double x[size], r[size], p[size], pA[size], aux[size], rnew, rold, alfa, beta;
+void conjugate_gradient(double A[][nmax], double b[], int size) {
+	double *x, *r, *p, *Ap, *aux, rnew, rold, alfa;
 	int i;
+
+    x = (double*) malloc(size*sizeof(double));
+    r = (double*) malloc(size*sizeof(double));
+    p = (double*) malloc(size*sizeof(double));
+    Ap = (double*) malloc(size*sizeof(double));
+    aux = (double*) malloc(size*sizeof(double));
 
 	for (i = 0; i < size; i ++) {
 		x[i] = 0;
@@ -76,32 +76,39 @@ void conjugate_gradient(double A[][nmax], double b[], int size, int n_steps) {
 	}
 
 	rold = inner_product(r, r, size);
-
-	for (i = 1; i < n_steps; i ++) {
-		matrix_vector_product(A, p, size, pA); /* resposta no pA */
-		alfa = rold / inner_product(p, pA, size);
-		vector_by_a_scalar_new_vector(p, alfa, size, aux); 
-		vector_sum(x, aux, size); /* atualiza o próprio x */
-		vector_by_a_scalar_new_vector(pA, alfa, size, aux);
-		vector_subtraction(r, aux, size); /* atualiza o próprio r */
+    /* result of operations from all void functions are saved at last argument */
+	for (i = 1; i < size; i ++) {
+		matrix_vector_product(A, p, size, Ap);
+		alfa = rold / inner_product(p, Ap, size); /*step length*/
+		vector_scalar_product(p, alfa, size, aux); 
+		vector_sum(x, aux, size, x);
+		vector_scalar_product(Ap, alfa, size, aux);
+		vector_subtraction(r, aux, size, r);
 		rnew = inner_product(r, r, size);
-		if (sqrt(rnew) < 1e-10)
+		if (sqrt(rnew) < E)
             break;
 
-        vector_sum(vector_by_a_scalar(p ,rnew / rold, size), r, size), size, 1);
+        vector_scalar_product(p, rnew / rold, size, p);
+        vector_sum(p, r, size, p);
 		rold = rnew;
 	}
 
 	for (i = 0; i < size; i ++) {
 		b[i] = x[i];
 	}
+
+	free(x);
+	free(r);
+	free(p);
+	free(Ap);
+	free(aux);
 }
 
 int main() {
 	char file_name[100];
 	FILE *file;
 	double A[nmax][nmax], b[nmax], duration;
-	int n, i, j, k, p[nmax];
+	int n, i, j, k;
 	clock_t start, end;
 
 	printf("Nome do Arquivo: ");
@@ -124,14 +131,14 @@ int main() {
 	}
 
 	start = clock();
-	conjugate_gradient(A, b, n, n_steps);
+	conjugate_gradient(A, b, n);
 	end = clock();
 	duration = (double)(end - start) / CLOCKS_PER_SEC;
 	printf("conjugate_gradient tempo %e segundos\n", duration);
 
 	for (i = 0; i < n; i ++) {
 		if (b[i] - (1 + i%(n/100)) > E || b[i] - (1 + i%(n/100)) < -E)
-			printf("Erro! %e  %d %d\n", b[i],-(1 + i%(n/100)), i);
+			printf("Erro! %e  %d %d\n", b[i], - (1 + i%(n/100)), i);
 	}
 	printf("Fim da Análise!\n");
 	return 0;
