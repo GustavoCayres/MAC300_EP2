43d943e673e72aa89a9a07be349d75655e3583df
Gustavo Cayres <gustavocayrespublico@gmail.com>
2015-10-31 03:32:18 +0000

    funcionando, necessarios mais testes

--- ep2.c
+++ ep2.c
@@ -6,20 +6,20 @@
 #define E 1e-10 /* Acceptable error */
 /* Compilation command: gcc -Wall -Wextra -ansi -pedantic -o ep2 ep2.c -lm */
 
-/* minha ideia é implementar a matrix como uma lista de lista, como a operação que vamos fazer é Axp, entao guardando
-linha por linha, fica mais facil de implementar essa operação, e para ler do arquivo vai ser fácil */
-struct column{
+/* use of structs to implement linked list nodes */
+/* the sparse matrix will be represented by a list of lists */
+struct col{
 	int col;
 	double value;
-	struct column *right;
-}column;
+	struct col *right;
+}col;
 
-typedef struct column* coluna;
+typedef struct col* column;
 
 struct row{
 	int row;
 	struct row *down;
-	struct column *right;
+	struct col *right;
 }row;
 
 typedef struct row* sparse_matrix;
@@ -35,38 +35,38 @@ void create_matrix(sparse_matrix *sp) {
 
 void insert(sparse_matrix sp, int i, int j, double value) {
 	sparse_matrix rnew;
-	coluna cnew, pointer;
+	column cnew, pointer;
 	sparse_matrix aux;
 	if(sp->row == -1) {
 		sp->row = i;
-		cnew = malloc(sizeof(column));
+		cnew = malloc(sizeof(col));
 		cnew->col = j;
 		cnew->value = value;
 		cnew->right = NULL;
 		sp->right = cnew;
 	}
 	else {
-		cnew = malloc(sizeof(column));
+		cnew = malloc(sizeof(col));
 		cnew->col = j;
 		cnew->value = value;
 		cnew->right = NULL;
-		for(aux = sp; aux->down != NULL && aux->row != i; aux = aux->down);
-		if(aux->down == NULL) {
+		for(aux = sp; aux->row != i && aux->down != NULL; aux = aux->down);
+		if(aux->row == i) {
+			for(pointer = aux->right; pointer->right != NULL; pointer = pointer->right);
+			pointer->right = cnew;
+		}
+		else {
 			rnew = malloc(sizeof(row));
 			rnew->row = i;
 			rnew->down = NULL;
 			rnew->right = cnew;
 			aux->down = rnew;
 		}
-		else{
-			for(pointer = aux->right; pointer->right != NULL; pointer = pointer->right);
-			pointer->right = cnew;
-		}
 	}
 }
 
-void free_right(coluna c){
-	coluna atual, next;
+void free_right(column c){
+	column atual, next;
 	atual=c;
 	while(atual != NULL){
 		next = atual->right;
@@ -106,13 +106,15 @@ double inner_product(double v1[], double v2[], int size) {
 }
 
 /* calculates the multiplication between a matrix and a vector */
-void matrix_vector_product(double A[][nmax], double v[], int size, double result[]) {
-	int i, j;
+void matrix_vector_product(sparse_matrix A, double v[], double result[]) {
+	sparse_matrix aux;
+	column pointer;
 
-	for (i = 0; i < size; i ++) {
-		result[i] = 0;
-		for (j = 0; j < size; j ++)
-			result[i] += A[i][j]*v[j];
+	for(aux = A; aux != NULL; aux = aux->down) {
+		result[aux->row] = 0;
+		for(pointer = aux->right; pointer != NULL; pointer = pointer->right) {
+			result[aux->row] += (pointer->value)*v[pointer->col];
+		}
 	}
 }
 
@@ -140,7 +142,8 @@ void vector_subtraction(double v1[], double v2[], int size, double result[]) {
 		result[i] = v1[i] - v2[i];
 }
 
-void conjugate_gradient(double A[][nmax], double b[], int size) {
+/*solution saved in b[] */
+void conjugate_gradient(sparse_matrix A, double b[], int size) {
 	double *x, *r, *p, *Ap, *aux, rnew, rold, alfa;
 	int i;
 
@@ -157,12 +160,9 @@ void conjugate_gradient(double A[][nmax], double b[], int size) {
 	}
 
 	rold = inner_product(r, r, size);
-    /* result of operations from all void functions are saved at last argument */
-	/* for (i = 1; i < size; i ++) {*/
-	i = 0;
+    /* result of operations from all void functions used here are stored in the last argument */
 	while (1) {
-		i ++;
-		matrix_vector_product(A, p, size, Ap);
+		matrix_vector_product(A, p, Ap);
 		alfa = rold / inner_product(p, Ap, size); /*step length*/
 		vector_scalar_product(p, alfa, size, aux); 
 		vector_sum(x, aux, size, x);
@@ -192,24 +192,31 @@ void conjugate_gradient(double A[][nmax], double b[], int size) {
 int main() {
 	char file_name[100];
 	FILE *file;
-	double A[nmax][nmax], b[nmax], duration;
+	sparse_matrix A;
+	double b[nmax], duration, aux;
 	int n, i, j, k;
 	clock_t start, end;
-
+	
+	A = malloc(sizeof(row));
+	A->down = NULL;
+	A->right = NULL;
+	A->row = -1; 
 	printf("Nome do Arquivo: ");
 	scanf("%s", file_name);
 	file = fopen(file_name, "r");
 
-	/* Lendo dados */
+	/* Reading file */
 	if (file == NULL) {
 		fprintf(stderr, "Não foi possível abrir o arquivo!\n");
 		return -1;
 	}
+
 	fscanf(file, "%d", &n);
 
 	for (k = 0; k < n*n; k ++) {
 		fscanf(file, "%d %d", &i, &j);
-		fscanf(file, "%lf", &A[i][j]);
+		fscanf(file, "%lf", &aux);
+		insert(A, i, j, aux);
 	}
 	for (k= 0; k < n; k ++) {
 		fscanf(file, "%d", &i);
@@ -220,8 +227,9 @@ int main() {
 	conjugate_gradient(A, b, n);
 	end = clock();
 	duration = (double)(end - start) / CLOCKS_PER_SEC;
-	printf("conjugate_gradient tempo %e segundos\n", duration);
-
+	printf("Tempo de resolução por Gradientes Conjugados: %e segundos\n", duration);
+        
+        /* test */
 	for (i = 0; i < n; i ++) {
 		if (b[i] - (1 + i%(n/100)) > 1e-5 || b[i] - (1 + i%(n/100)) < -1e-5)
 			printf("Erro! %e  %d %d\n", b[i], (1 + i%(n/100)), i);
